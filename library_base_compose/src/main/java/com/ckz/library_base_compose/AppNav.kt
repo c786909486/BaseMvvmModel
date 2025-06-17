85b65a61c1961415fdeafa9a443767d6c3549e23
ckz <18869978285@163.com>
2025-06-17 09:01:56 +0800

    插件修改

--- library_base_compose/src/main/java/com/ckz/library_base_compose/AppNav.kt
+++ library_base_compose/src/main/java/com/ckz/library_base_compose/AppNav.kt
@@ -1,10 +1,20 @@
 package com.ckz.library_base_compose
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+
 
 
 /**
@@ -40,7 +50,32 @@ class AppNav {
         navController: NavHostController,
         startDestination: String,
         modifier: Modifier = Modifier,
+        contentAlignment: Alignment = Alignment.TopStart,
         route: String? = null,
+        enterTransition:
+        (@JvmSuppressWildcards
+        AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition) =
+            {
+                fadeIn(animationSpec = tween(700))
+            },
+        exitTransition:
+        (@JvmSuppressWildcards
+        AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition) =
+            {
+                fadeOut(animationSpec = tween(700))
+            },
+        popEnterTransition:
+        (@JvmSuppressWildcards
+        AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition) =
+            enterTransition,
+        popExitTransition:
+        (@JvmSuppressWildcards
+        AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition) =
+            exitTransition,
+        sizeTransform:
+        (@JvmSuppressWildcards
+        AnimatedContentTransitionScope<NavBackStackEntry>.() -> SizeTransform?)? =
+            null,
         builder: NavGraphBuilder.() -> Unit
     ) {
         _appNavController = navController
@@ -48,7 +83,13 @@ class AppNav {
             navController,
             startDestination,
             modifier,
+            contentAlignment,
             route,
+            enterTransition,
+            exitTransition,
+            popEnterTransition,
+            popExitTransition,
+            sizeTransform,
             builder
         )
     }
--- library_base_compose/src/main/java/com/ckz/library_base_compose/base/BaseViewModel.kt
+++ library_base_compose/src/main/java/com/ckz/library_base_compose/base/BaseViewModel.kt
@@ -1,35 +1,9 @@
 package com.ckz.library_base_compose.base
 
-import android.app.Application
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.window.SecureFlagPolicy
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -47,7 +21,7 @@ import org.greenrobot.eventbus.ThreadMode
 abstract class BaseViewModel() : ViewModel(),
     IBaseViewModel {
 
-    var isDialog = MutableLiveData(false)
+    var isDialog by mutableStateOf(false)
 
     var dialogText by mutableStateOf("")
 
@@ -55,7 +29,8 @@ abstract class BaseViewModel() : ViewModel(),
 
     var canDismissByBack by mutableStateOf(true)
 
-    var pageData = MutableLiveData(PageState.CONTENT.bindData())
+    var pageData by mutableStateOf(PageState.CONTENT.bindData())
+
     open val unregisterOnStop get() = false
 
     var hasCreated = false
@@ -65,160 +40,24 @@ abstract class BaseViewModel() : ViewModel(),
      */
     private var mInjectModels: MutableList<BaseModel?> = ArrayList()
 
-//    init {
-//        initModel()
-//    }
-//
-//    private fun initModel() {
-//        val fields = this.javaClass.declaredFields
-//        for (field in fields) {
-//            val injectModel = field.getAnnotation(InjectModel::class.java)
-//            if (injectModel != null) {
-//                try {
-//                    val type: Class<out BaseModel?> =
-//                        field.type as Class<out BaseModel?>
-//                    val mInjectModel = type.newInstance()
-//                    field.isAccessible = true
-//                    field.set(this, mInjectModel)
-//                    mInjectModels.add(mInjectModel)
-//                } catch (e: IllegalAccessException) {
-//                    e.printStackTrace();
-//                } catch (e: InstantiationException) {
-//                    e.printStackTrace();
-//                } catch (e: ClassCastException) {
-//                    e.printStackTrace()
-//                    throw RuntimeException("SubClass must extends Class:BaseModel");
-//                }
-//            }
-//        }
-//    }
-
-//    @Composable
-//    fun ContentWidget(
-//        modifier: Modifier = Modifier,
-//        onRetry: OnRetry = { retryClick() },
-//        loading: @Composable (StateLayoutData) -> Unit = { DefaultLoadingLayout(it) },
-//        empty: @Composable (StateLayoutData) -> Unit = { DefaultEmptyLayout(it) },
-//        error: @Composable (StateLayoutData) -> Unit = { DefaultErrorLayout(it) },
-//        content: @Composable () -> Unit = { },
-//
-//        ) {
-//        val showDialog = isDialog.observeAsState()
-//        val pageStateData = pageData.observeAsState()
-//        val lifecycleOwner = LocalLifecycleOwner.current
-//        val lifecycle = lifecycleOwner.lifecycle
-//
-//        val observer = remember {
-//            LifecycleEventObserver { owner, event ->
-//                when (event) {
-//                    Lifecycle.Event.ON_CREATE -> {
-//                        if (!hasCreated) {
-//                            onCreate(owner)
-//                            hasCreated = true
-////                            Log.d("HomePage", "HomePage: ${event.name}")
-//                        }
-//
-//                    }
-//
-//                    Lifecycle.Event.ON_START -> {
-//                        onStart(owner)
-////                        Log.d("HomePage", "HomePage: ${event.name}")
-//                    }
-//
-//                    Lifecycle.Event.ON_RESUME -> {
-//                        onResume(owner)
-////                        Log.d("HomePage", "HomePage: ${event.name}")
-//                    }
-//
-//                    Lifecycle.Event.ON_PAUSE -> {
-////                        onPause(owner)
-//                        Log.d("HomePage", "HomePage: ${event.name}")
-//                    }
-//
-//                    Lifecycle.Event.ON_STOP -> {
-//                        onStop(owner)
-////                        Log.d("HomePage", "HomePage: ${event.name}")
-//                    }
-//
-//                    Lifecycle.Event.ON_DESTROY -> {
-//                        onDestroy(owner)
-//                        onCleared()
-////                        Log.d("HomePage", "HomePage: ${event.name}")
-//                        hasCreated = false
-//                    }
-//
-//                    Lifecycle.Event.ON_ANY -> {
-//                        onAny(owner, event)
-////                        Log.d("HomePage", "HomePage: ${event.name}")
-//                    }
-//                }
-//
-//            }
-//        }
-//
-//        DisposableEffect(lifecycleOwner, observer) {
-//            lifecycle.addObserver(observer)
-//            onDispose {
-//                lifecycle.removeObserver(observer)
-//            }
-//        }
-//
-//        DefaultStateLayout(
-//            modifier = modifier,
-//            pageStateData = pageStateData.value!!,
-//            onRetry = onRetry,
-//            loading = loading,
-//            empty = empty,
-//            error = error,
-//            content = content
-//        )
-//
-//        if (true == showDialog.value) {
-//            Dialog(
-//                onDismissRequest = {
-//                    isDialog.value = !isDialog.value!!
-//                },
-//                properties = DialogProperties(
-//                    dismissOnClickOutside = canDismissByTouch,
-//                    usePlatformDefaultWidth = false,
-//                    decorFitsSystemWindows = false,
-//                    dismissOnBackPress = canDismissByBack//
-//                )
-//            ) {
-//                Column(
-//                    modifier = Modifier
-//
-//                        .clip(RoundedCornerShape(6.dp))
-//                        .background(Color.Black.copy(alpha = 0.5f))
-//                        .padding(12.dp),
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    CircularProgressIndicator(color = Color.White)
-//                    Box(modifier = Modifier.height(10.dp))
-//                    Text(text = dialogText, color = Color.White)
-//                }
-//
-//            }
-//        }
-//    }
 
     fun showContent() {
-        pageData.value = PageState.CONTENT.bindData()
+        pageData = PageState.CONTENT.bindData()
     }
 
     fun showLoading(msg: String = "") {
         val setData = StateData(tipTex = msg)
-        pageData.value = PageState.LOADING.bindData(setData)
+        pageData = PageState.LOADING.bindData(setData)
     }
 
     fun showEmpty(emptyMsg: String = "暂无数据") {
         val setData = StateData(tipTex = emptyMsg, btnText = "点击重试")
-        pageData.value = PageState.EMPTY.bindData(setData)
+        pageData = PageState.EMPTY.bindData(setData)
     }
 
     fun showError(errorMsg: String) {
         val setData = StateData(tipTex = errorMsg, btnText = "点击重试")
-        pageData.value = PageState.ERROR.bindData(setData)
+        pageData = PageState.ERROR.bindData(setData)
     }
 
     fun showProcessDialog(
@@ -226,14 +65,14 @@ abstract class BaseViewModel() : ViewModel(),
         canTouchDismiss: Boolean = false,
         canTouchByBack: Boolean = true
     ) {
-        isDialog.value = true
+        isDialog = true
         dialogText = msg
         canDismissByTouch = canTouchDismiss
         canDismissByBack = canTouchByBack
     }
 
     fun hideDialog() {
-        isDialog.value = false
+        isDialog = false
     }
 
     open fun retryClick() {
